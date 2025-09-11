b4a66997af94c1fa20a02470e09ec449f0984bbb
Salil-IND <salil_programming@outlook.com>
2025-09-11 20:08:42 +0530

    Added a new integration with dino game(Game included)

---
+++ Build_Day(Gesture_Control_Gaming)/chrome-dino-game/README.md
@@ -0,0 +1,10 @@
+This is a dino runner game based on pygame which is controlled by gestures of the hands. 
+
+**How to play**
+- Show your entire palm (such that all fingers are standing) to make the dino jump
+- Show any two fingers (whichever configuration youre comfortable with) to make the dino duck. Also you have to make it jump again using your palms to un-duck it.
+
+**Advices**
+- As the game is not fully optimised for experience and only for experimenting purposes, the game might not always function properly.
+- Please make sure to make the gesture early as the hitboxes of the obstacles and the dino are large and the time to recognise the gesture also adds up to a small but significant time lag affecting gaming experience.
+- Crossing even 100 on this would be considered an achievement because of its its complicated gameplay. Lmao
--- Build_Day(Gesture_Control_Gaming)/chrome-dino-game/dino_gesture_control.py
+++ Build_Day(Gesture_Control_Gaming)/chrome-dino-game/dino_gesture_control.py
@@ -1,10 +1,9 @@
-# pip install opencv-contrib-python
-# pip install cvzone
-# pip install pyautogui
 
-import cv2
-from cvzone.HandTrackingModule import HandDetector
-import pyautogui
+
+# pip install -r requirements.txt
+
+import cv2, pyautogui, time
+from cvzone.HandTrackingModule import HandDetector 
 
 detector = HandDetector(detectionCon=0.5, maxHands=2)
 cap = cv2.VideoCapture(0)
@@ -14,16 +13,19 @@ while True:
     success, img = cap.read()
     img = cv2.flip(img, 1)
     hand, img = detector.findHands(img)
-    if hand and hand[0]['type'] == "Right":        
+    if hand and hand[0]['type'] == "Left":        
         fingers = detector.fingersUp(hand[0])
         totalFingers = fingers.count(1)
         cv2.putText(img, f'Fingers: {totalFingers}', (50, 50), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
         if totalFingers == 5:
-            pyautogui.keyDown('right')
-            pyautogui.keyUp('left')
-        if totalFingers ==0:
-            pyautogui.keyDown('left')
-            pyautogui.keyUp('right')
+            pyautogui.keyUp('down')
+            pyautogui.keyDown('up')
+            time.sleep(0.5)                
+            pyautogui.keyUp('up')
+        if totalFingers ==2:
+            pyautogui.keyDown('down')                       
+            
 
     cv2.imshow('Camera Feed', img)
     cv2.waitKey(1)
+
